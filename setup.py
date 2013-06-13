42b6a167b41e753be164ac1818077ee743340552
Mark Fiers <mark.fiers.42@gmail.com>
2013-06-13 16:42:33 +0200

    bumped version

--- setup.py
+++ setup.py
@@ -10,19 +10,32 @@ Yaco objects allowing similar access (`a.key.subkey`). Lists are
 objects allowing access allong the lines of `a.key[3].subkey`."""
 
 from setuptools import setup
+from setuptools.command.test import test as TestCommand
+import sys
+
+class Tox(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+    def run_tests(self):
+        #import here, cause outside the eggs aren't loaded
+        import tox
+        errno = tox.cmdline(self.test_args)
+        sys.exit(errno)
 
 DESCRIPTION = "YAML serializable dict like object with attribute style access and implicit branch creation"
 
 setup(name='Yaco',
-      version='0.1.17',
+      version='0.1.19',
       description=DESCRIPTION,
       author='Mark Fiers',
       author_email='mark.fiers42@gmail.com',
       url='https://github.com/mfiers/Yaco',
       packages=['Yaco'],
-      requires = [
-        'PyYAML (>3.0)',
-        ],
+      install_requires = ['PyYAML>=3.0'],
+      tests_require = ['tox'],
+      cmdclass = {'test': Tox},
       classifiers = [
           'Development Status :: 5 - Production/Stable',
           'Intended Audience :: Developers',
